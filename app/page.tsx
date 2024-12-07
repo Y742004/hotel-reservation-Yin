d9bf2e7096b7fab5fa29fc8c039c839e99623a79
Moe San <starlightastro@Moes-MacBook-Pro.local>
2024-12-07 12:00:04 -0500

    hotel

--- app/page.tsx
+++ app/page.tsx
@@ -52,6 +52,7 @@ export default function Page() {
   return (
     <>
       <div className="p-5">
+        <h1>hi</h1>
  <div className=""> <ThemeSwitcher/></div>
 
         <div className="p-8 border rounded-xl w-[500px] mx-auto mt-10">
